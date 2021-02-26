fd00d2372c6600929f4b94489ce60244a3b2c0a5
Timm Bahmann <timm@bahmann.one>
2021-02-26 04:08:40 +0100

    fix(component): Fix storyboard test
    I changed the name of the click function, but forgot to change
    it in the story, too.
    
    Closed #14

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -25,7 +25,7 @@ export default {
 const Template = (_args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { MyButton },
-  template: '<my-button @onClick="onClick" v-bind="$props" />',
+  template: '<my-button @onClick="click" v-bind="$props" />',
 })
 
 export const Primary = Template.bind({})
